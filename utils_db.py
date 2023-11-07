1075e65203f84f2d8c2d3e19d92d03389ddd3b42
x-Four-x <124124904+x-Four-x@users.noreply.github.com>
2023-11-07 21:50:03 +0300

    Add comment

--- utils_db.py
+++ utils_db.py
@@ -2,21 +2,26 @@ from models import Note
 
 
 def create_table() -> None:
+    """Создает таблицу в базе данных"""
     Note.create_table()
 
 def get_note(note_id: int) -> Note | None:
+    """Возвращает обьект записи в базе данных или None"""
     return Note.get_or_none(
         note_id=note_id
     )
 
 def create_note(title: str, description: str) -> Note:
+    """Создает заметку в базе данных"""
     return Note.create(
         title=title, 
         description=description
     )
 
 def delete_note(note_id: int) -> None:
+    """Удаляет заметку в базе данных"""
     Note.delete_by_id(note_id)
 
 def get_all_notes() -> list[Note]:
+    """Возвращает все имеющиеся заметки в базе данных"""
     return Note.select()
